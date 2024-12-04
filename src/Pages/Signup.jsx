def765305952b76464003cf1cccb807785733c1f
showrov4g <showrovg00@gmail.com>
2024-12-04 11:28:12 +0600

    successfully user create

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,61 +1,78 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from "../Provider/AuthProvider";
 import { toast } from "react-toastify";
 
 const Signup = () => {
+  const { createUser, user, setUser } = useContext(AuthContext);
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data.name, data.password);
+  const onSubmit = (data) => {
+    
+    const {email, password} = data;
+console.log(email, password);
+    createUser(email,password)
+      .then((result) => {
+        toast.success("You have successfully create your account");
+        console.log(result);
+      })
+   .catch(err=>{
+    toast.error(`Something went wrong ${err}`);
+   })
+  };
 
   return (
     <div className="py-10 bg-gray-500 flex justify-center items-center">
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-        <label htmlFor="name" className="text-2xl !text-start">Enter your Name</label>
+        <label htmlFor="name" className="text-2xl !text-start">
+          Enter your Name
+        </label>
         <input
-        className="text-3xl p-3 rounded-xl"
+          className="text-3xl p-3 rounded-xl"
           type="text"
           placeholder="Name"
           {...register("name", { required: true })}
-          aria-invalid={errors.name ? "true" : "false"} 
-        />{
-            errors.name?.type === 'required' && <p role="alert">First name is required</p>
-        }
+          aria-invalid={errors.name ? "true" : "false"}
+        />
+        {errors.name?.type === "required" && (
+          <p role="alert">First name is required</p>
+        )}
         <label htmlFor="email">Enter your email</label>
         <input
-        className="text-3xl p-3 rounded-xl"
+          className="text-3xl p-3 rounded-xl"
           type="email"
           placeholder="Email"
           {...register("email", { required: true })}
-          aria-invalid={errors.email ? "true" : "false"} 
+          aria-invalid={errors.email ? "true" : "false"}
         />
-        {
-            errors.email?.type === 'required' && <p role="alert">First name is required</p>
-        }
+        {errors.email?.type === "required" && (
+          <p role="alert">First name is required</p>
+        )}
         <label htmlFor="Photo url">Enter your Photo URL</label>
         <input
-        className="text-3xl p-3 rounded-xl"
+          className="text-3xl p-3 rounded-xl"
           type="url"
           placeholder="Photo URL"
           {...register("PhotoURL", { required: true })}
         />
         <label htmlFor="password">Set your password</label>
         <input
-        className="text-3xl p-3 rounded-xl"
+          className="text-3xl p-3 rounded-xl"
           type="password"
           placeholder="password"
           {...register("password", {
             max: 10,
             min: 6,
-            pattern:
-              /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
+            pattern: /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
           })}
-          aria-invalid={errors.password ? "true" : "false"} 
+          aria-invalid={errors.password ? "true" : "false"}
         />
-                {
-            errors.password?.type === 'required' && <p role="alert">First name is required</p>
-        }
+        {errors.password?.type === "required" && (
+          <p role="alert">First name is required</p>
+        )}
 
         <input type="submit" />
       </form>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,14 +1,14 @@
+
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { Children, createContext, useState } from "react";
-import { auth } from "../Firebale/Firebase.init";
+import { createContext, useState } from "react";
 import { toast } from "react-toastify";
+import { auth } from "../Firebale/Firebase.init";
 
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState();
 
   const createUser = (email, password) => {
-    toast.success("You have successfully create your account");
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
@@ -19,7 +19,9 @@ const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={authInfo}>
+        {children}
+    </AuthContext.Provider>
   );
 };
 
