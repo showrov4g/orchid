ac33ca48f3307aea5d333e52c51bf555eb4d5715
showrov4g <showrovg00@gmail.com>
2024-12-04 08:53:01 +0600

    sign up form making

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,11 +1,55 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import { toast } from "react-toastify";
 
 const Signup = () => {
-    return (
-        <div>
-            <h1>Sign up</h1>
-        </div>
-    );
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+  console.log(errors);
+
+  return (
+    <div className="py-10 bg-gray-500 ">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Name"
+          {...register("name", { required: true })}
+          aria-invalid={errors.name ? "true" : "false"} 
+        />{
+            errors.name?.type === 'required' && toast("Name must enter")
+        }
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: true })}
+          aria-invalid={errors.name ? "true" : "false"} 
+        />
+        {
+            errors.email?.type === 'required' && toast("Name must enter")
+        }
+        <input
+          type="url"
+          placeholder="Photo URL"
+          {...register("Photo URL", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          {...register("password", {
+            max: 10,
+            min: 6,
+            pattern:
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+          })}
+        />
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default Signup;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Route from './Route/Route.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Route from "./Route/Route.jsx";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Route></Route>
-  </StrictMode>,
-)
+    <ToastContainer />
+  </StrictMode>
+);
