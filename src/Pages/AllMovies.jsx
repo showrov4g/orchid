9c453f1ccce4c805c034404cf0c95595c271f55d
showrov4g <showrovg00@gmail.com>
2024-12-05 20:31:54 +0600

    making All movies page

--- src/Pages/AllMovies.jsx
+++ src/Pages/AllMovies.jsx
@@ -1,13 +1,33 @@
-import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 const AllMovies = () => {
-    const moviesData = useLoaderData();
-    return (
-        <div>
-            
-        </div>
-    );
+  const moviesData = useLoaderData();
+
+  return (
+    <div>
+      <h1>All movies</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {moviesData.map((movie) => (
+          <div className="card card-compact bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AllMovies;
