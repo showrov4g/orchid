45d4c7a4b3b745715e34fe991661e5bfc916fe0b
showrov4g <showrovg00@gmail.com>
2025-01-27 11:48:19 +0600

    env file setup

--- src/Firebale/Firebase.init.js
+++ src/Firebale/Firebase.init.js
@@ -7,15 +7,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCLYZtCTck_JicMuJA2NxYJ_prXTOb4b1w",
-  authDomain: "assaignment10.firebaseapp.com",
-  projectId: "assaignment10",
-  storageBucket: "assaignment10.firebasestorage.app",
-  messagingSenderId: "837506960911",
-  appId: "1:837506960911:web:67c3978370e8ee676e9d7a",
-  measurementId: "G-RVH0P21DTL"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
